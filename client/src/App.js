83337a93c4bd85fee06f3d249f55736302e3ee99
Shadiyar  Alakhan <shadiyar.alakhan@gmail.com>
2022-09-11 17:11:15 +0600

    Websocket server/client side integrated and working correctly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Longpulling from "./longpulling";
+import WebSock from "./WebSock";
 
 function App() {
   return (
     <div >
-      <Longpulling></Longpulling>
+      <WebSock/>
         <div></div>
     </div>
   );
