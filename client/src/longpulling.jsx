cabafc26d17be413ef615222022e068ac4352164
Shadiyar  Alakhan <shadiyar.alakhan@gmail.com>
2022-09-11 15:03:51 +0600

    i to day finaly finish Real time Chat With Longpulling ! Unfortunaly  i have some bugs  but i will fix it later.

--- client/src/longpulling.jsx
+++ client/src/longpulling.jsx
@@ -1,17 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
-
-const Longpulling=()=>{
+const LongPulling = () => {
     const [messages, setMessages] = useState([]);
     const [value, setValue] = useState('');
 
-    return(
+
+    useEffect(() =>{ subscribe()
+    }, [])
+
+    const subscribe = async () => {
+        try {
+            const {data} = await axios.get('http://localhost:5000/get-messages')
+            setMessages(prev => [data, ...prev])
+            await subscribe()
+        } catch (e) {
+            setTimeout(() => {
+                subscribe()
+            },1500)
+        }
+    }
+
+    const sendMessage = async () => {
+        await axios.post('http://localhost:5000/new-messages', {
+            message: value,
+            id: Date.now()
+        })
+    }
+
+    return (
         <div className="center">
             <div>
                 <div className="form">
                     <input value={value} onChange={e => setValue(e.target.value)} type="text"/>
-
-                    <button >Отправить</button>
+                    <button onClick={sendMessage}>Отправить</button>
                 </div>
                 <div className="messages">
                     {messages.map(mess =>
@@ -22,6 +44,7 @@ const Longpulling=()=>{
                 </div>
             </div>
         </div>
-    )
-}
-export  default Longpulling;
+    );
+};
+
+export default LongPulling;
--- server/longpulling.js
+++ server/longpulling.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const events = require('events');
-
-const emiter = new events.EventEmitter()
+const events = require('events')
 const PORT = 5000;
 
-const app = express();
+const emitter = new events.EventEmitter();
+
+const app = express()
+
 app.use(cors())
+app.use(express.json())
 
-app.get('get-message', ((req, res)=>{
-      emiter.once('newMessage', (message)=>{
-          res.json(message)
-      })
-}))
-
-app.post('new-message', ((req, res)=>{
-      const message = req.body;
-      emiter.emit('newMessage', message)
-      res.status(200);
-}))
-
-app.listen(PORT, ()=>{
-    console.log(`Server start on port ${PORT}`);
+app.get('/get-messages', (req, res) => {
+    emitter.once('newMessage', (message) => {
+        res.json(message)
+    })
 })
+
+app.post('/new-messages', ((req, res) => {
+    const message = req.body;
+    emitter.emit('newMessage', message)
+    res.status(200)
+}))
+
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
